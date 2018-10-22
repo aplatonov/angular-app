d7bca09059a0cd1ab09e37d24599f499795b544d
Anthony Platonov <a.platonov@php73.ru>
2018-10-22 16:54:07 +0400

    2 task: in progress v1.01

---
+++ src/app/mock-users.ts
@@ -0,0 +1,14 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+  { id: 11, secondname: 'Одиннадцатый', name: 'Mr. Nice', surname: 'Иваныч' },
+  { id: 12, secondname: 'Двенадцатый', name: 'Narco', surname: 'Петрович' },
+  { id: 13, secondname: 'Тринадцатый', name: 'Bombasto', surname: 'Рафаэлеч' },
+  { id: 14, secondname: 'Четырнадцатый', name: 'Celeritas', surname: 'Копатыч' },
+  { id: 15, secondname: 'Пятнадцатый', name: 'Magneta', surname: 'Батоныч' },
+  { id: 16, secondname: 'Шестнадцатый', name: 'RubberMan', surname: 'Антоныч' },
+  { id: 17, secondname: 'Семнадцатый', name: 'Dynama', surname: 'Магарыч' },
+  { id: 18, secondname: 'Восемнадцатый', name: 'Dr IQ', surname: 'Андревна' },
+  { id: 19, secondname: 'Девятнадцатый', name: 'Magma', surname: 'Василишна' },
+  { id: 20, secondname: 'Двадцатый', name: 'Tornado', surname: 'Красавишна' }
+];
---
+++ src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+	id: number;
+	secondname: string;
+	name: string;
+	surname: string;
+}
